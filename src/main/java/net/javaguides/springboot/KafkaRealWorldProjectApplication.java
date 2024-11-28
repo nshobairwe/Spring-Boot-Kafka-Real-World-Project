7eef0b59216327fd766a5d31984a3307c8945f3a
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2024-11-28 19:30:11 +0300

    first wikimedia producer module created

---
+++ src/main/java/net/javaguides/springboot/KafkaRealWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRealWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRealWorldProjectApplication.class, args);
+	}
+
+}
